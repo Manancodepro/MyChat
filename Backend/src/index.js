119e0fef3f551ed82a30e16896b8fcb90b2ec2c7
Manan Patel <mspcodepro1506@gmail.com>
2026-02-26 09:32:52 +0530

    changes

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -54,7 +54,7 @@ app.get("/", (req, res) => {
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-  app.get("/:path(*)", (req, res) => {
+  app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
   });
 }
